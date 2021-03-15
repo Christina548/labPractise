d08af00e0f4f65d5d262c5b80fcaf84ba986f74e
Christina <christina-hardy@live.nmit.ac.nz>
2021-03-16 12:42:24 +1300

    index.js updates

---
+++ index.js
@@ -0,0 +1,56 @@
+Number (123)
+console.log
+x = '123456'
+console.log (typeof x)
+let y = Number (x)
+// console log ()
+console.log (typeof y)
+const a = 1+1
+console.log (a)
+
+const b = a + 2
+console.log (b)
+
+let name 
+console.log (name)
+
+const information = {
+    PLace : 'Wakatahuri',
+    Country : 'New Zealand',
+    Boat : 'Winsome',
+} 
+console.log (information)
+
+let c = 11
+
+if (c === 12 && typeof c === 'number') {
+    console.log ('its true')
+} else {
+    console.log ('error')
+}
+
+let c = 11
+
+if (c === 11 || typeof c === 'string') {
+    console.log ('its true')
+} else {
+    console.log ('error')
+}
+
+ let c = 11
+
+if (c !== 11) {
+    console.log ('its true')
+} else {
+    console.log ('error')
+}
+
+let a = 5
+let b = a
+console.log(a) //5
+console.log(b) //5
+console.log(a === b) // true
+a = 10
+console.log(a) // 10
+console.log(b) //5
+console.log (a === b) //false
