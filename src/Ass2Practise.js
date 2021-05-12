acb6593763ddbb87f1f6d18eafe386b124afc11a
Christina <christina-hardy@live.nmit.ac.nz>
2021-05-13 11:44:07 +1200

    changes to Ass2Practise

--- src/Ass2Practise.js
+++ src/Ass2Practise.js
@@ -1,24 +1,50 @@
-//first half - final 
-let input = [
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 1, 0, 1],
-    [1, 1, 0, 0],
-   ]
-const findTheMines = input.map(row => row.map(num => 
-num === 1 ? 'x' : 'y'));
-//draft of second part
-const countTheMines = findTheMines.map(row => row.map(num => 
-    num === 'x' ? 'x' : [num++]));
-    console.log(findTheMines([
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 1, 0, 1],
-            [1, 1, 0, 0],
-           ]))
+// // function game(arr) {
+// //     // Have a function to check the mines
+// //     function checkMine(param1, param2) {
+// //       // Your logic for finding mines
+// //     }
+    
+// //     function neighborsCounter(param1, param2) {
+// //       // Your logic for counting the numbers
+// //     }
+    
+// //     return the results
+// //   }
+function game(arr) {
+    let result = [[]]
+    function findTheMines2 (arr) {
+        arr.forEach(element => {element === 1 ? 'x' : 'y'})
+    } 
+    function neighborsCounter(arr) {
+        arr.forEach(element => {element === 'x' ? 'x' : x++})    
+    }
+     return result 
+}
+console.log(game([0,1,0,1]))
 
-//another draft of second part
-//     let result = []; 
+
+
+
+
+
+
+
+
+
+
+//first half - final 
+// let input = [
+//     [0, 1, 0, 0],
+//     [0, 0, 1, 0],
+//     [0, 1, 0, 1],
+// //     [1, 1, 0, 0],
+// //    ]
+// const findTheMines = input.map(row => row.map(num => 
+// num === 1 ? 'x' : 'y'));
+// findTheMines.forEach(element => {
+
+// })
+// let result = []; 
 //         for (let i = 0; i < input.length; i++) {
 //             let y = input[i][0];
 //             for (let J = 0; J < input[i].length; J++) {
@@ -28,4 +54,15 @@ const countTheMines = findTheMines.map(row => row.map(num =>
 //     return result;
 // }
 //         }
+
+
+// console.log(findTheMines2([[
+//         [0, 1, 0, 0],
+//         [0, 0, 1, 0],
+//         [0, 1, 0, 1],
+//         [1, 1, 0, 0],
+//        ]]))
+
+
+
 
